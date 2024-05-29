3e542336d10f3b58225eef75a9029063a26b40d9
Anubala85 <anushabalasubramaniann@gmail.com>
2024-05-29 10:14:00 -0700

    final version

--- schema.sql
+++ schema.sql
@@ -1,42 +1,68 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/S5AkKC
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 DROP TABLE IF EXISTS flights;
+DROP TABLE IF EXISTS aircraft;
 DROP TABLE IF EXISTS celebrity;
-DROP TABLE IF EXISTS classifications;
-DROP TABLE IF EXISTS airport;
+DROP TABLE IF EXISTS airports;
 DROP TABLE IF EXISTS averages;
 
-CREATE TABLE flights (
-	dep_airport VARCHAR(3) NOT NULL,
-	arr_airport VARCHAR(255) NOT NULL,
-    dep_date_time DATETIME,
-    fuel_gallons FLOAT,
-    celebrity_id VARCHAR NOT NULL,
-    tail_number INT,
-	PRIMARY KEY (tail_number)
+
+CREATE TABLE "flights" (
+    "dep_airport" varchar(4)   NOT NULL,
+    "arr_airport" varchar(4)   NOT NULL,
+    "dep_date_time" timestamp   NOT NULL,
+    "fuel_gallons" float   NULL,
+    "tail_number" varchar(32)   NOT NULL,
+    CONSTRAINT "pk_flights" PRIMARY KEY (
+        "dep_airport","arr_airport","dep_date_time","tail_number"
+     )
 );
 
-CREATE TABLE celebrity (
-	celebrity_id VARCHAR NOT NULL,
-    celebrity_name VARCHAR (255) NOT NULL,
-    celebrity_classification_id VARCHAR NOT NULL,
-    PRIMARY KEY (celebrity_id),
+CREATE TABLE "aircraft" (
+    "tail_number" varchar(32)   NOT NULL,
+    "celebrity_id" int   NOT NULL,
+    CONSTRAINT "pk_aircraft" PRIMARY KEY (
+        "tail_number"
+     )
 );
 
-CREATE TABLE classifications (
-	celebrity_classification_id VARCHAR NOT NULL,
-    celebrity_class VARCHAR NOT NULL,
-    PRIMARY KEY (celebrity_classification_id),
+CREATE TABLE "celebrity" (
+    "celebrity_id" int   NOT NULL,
+    "celebrity_name" varchar(255)   NOT NULL,
+    "celebrity_classification" varchar(255)   NULL,
+    CONSTRAINT "pk_celebrity" PRIMARY KEY (
+        "celebrity_id"
+     )
 );
 
-CREATE TABLE airports (
-	airport_code VARCHAR(4) NOT NULL,
-    airport_name VARCHAR NOT NULL,
-	longitude DEC,
-    latitude DEC,
-    PRIMARY KEY (airport_code),
+CREATE TABLE "airports" (
+    "airport_code" varchar(4)   NOT NULL,
+    "airport_name" varchar(255)   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_airports" PRIMARY KEY (
+        "airport_code"
+     )
 );
 
-CREATE TABLE averages (
-	gallons DEC,
-    miles DEC,
-    PRIMARY KEY (celebrity_classification_id),
+CREATE TABLE "averages" (
+    "gallons" float   NOT NULL,
+    "miles" dec   NOT NULL,
+    CONSTRAINT "pk_averages" PRIMARY KEY (
+        "gallons"
+     )
 );
+
+ALTER TABLE "flights" ADD CONSTRAINT "fk_flights_dep_airport" FOREIGN KEY("dep_airport")
+REFERENCES "airports" ("airport_code");
+
+ALTER TABLE "flights" ADD CONSTRAINT "fk_flights_arr_airport" FOREIGN KEY("arr_airport")
+REFERENCES "airports" ("airport_code");
+
+ALTER TABLE "flights" ADD CONSTRAINT "fk_flights_tail_number" FOREIGN KEY("tail_number")
+REFERENCES "aircraft" ("tail_number");
+
+ALTER TABLE "aircraft" ADD CONSTRAINT "fk_aircraft_celebrity_id" FOREIGN KEY("celebrity_id")
+REFERENCES "celebrity" ("celebrity_id");
+
---
+++ static/js/logic.js
@@ -0,0 +1,82 @@
+// Store our API endpoint as queryUrl.
+var queryUrl = "http://127.0.0.1:5000/flight_data";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function.
+    createMap(data);
+  });
+
+function markerSize (flight_count) {
+  console.log (`${flight_count}`);
+  return flight_count * 2000;
+}
+
+function markerColor (flight_count) {
+
+  if (flight_count <= 10) {
+    return "#00ff00"
+  } else if (flight_count <= 50) {
+      return "#ccff00"
+  } else if (flight_count <= 80) {
+      return "#ffae42"
+  } else if (flight_count <= 100) {
+      return "#f58025"
+  } else {
+      return "#ff4500"
+  }
+
+}
+
+function createMap (flightsData) {
+
+  // Create a map object.
+  let myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
+
+  // Add a tile layer.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
+  airport_dict = {};
+
+  for (let i = 0; i < flightsData.length; i++) {
+    let airport = flightsData[i].arr_airport;
+    let airport_name = flightsData[i].arr_airport_name;
+    let latitude = flightsData[i].arr_latitude;
+    let longitude = flightsData[i].arr_longitude;
+    
+    //console.log (`${airport}`)
+
+    if (airport_dict[airport] == null){
+      airport_dict[airport] = {};
+      airport_dict[airport]["name"] = airport_name;
+      airport_dict[airport]["flight_count"] = 0;
+      airport_dict[airport]["latitude"] = latitude;
+      airport_dict[airport]["longitude"] = longitude;
+    } else {
+      airport_dict[airport]["flight_count"] = airport_dict[airport]["flight_count"] + 1
+    }    
+  }
+
+  for (const key of Object.keys (airport_dict)) {
+    let airport_name = airport_dict[key]["name"];
+    let flight_count = airport_dict[key]["flight_count"];
+    let latitude = airport_dict[key]["latitude"];
+    let longitude = airport_dict[key]["longitude"];
+
+    L.circle([latitude, longitude], {
+      fillOpacity: 0.75,
+      color: "black",
+      weight: 0.8,
+      fillColor: markerColor (flight_count),
+      radius: markerSize(flight_count)
+    }).bindPopup(`<h3>${airport_name} (${key})</h3> <hr> <h4>Flights ${flight_count}</h4>`)
+      .addTo(myMap);
+  }
+
+}
+
